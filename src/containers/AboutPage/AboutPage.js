b6d8e146c18d1f2c272a5cd6b051960bdbdfe91f
manvirdosanjh <manvir.singh@live.ca>
2019-06-21 15:34:30 -0400

    Add files via upload

---
+++ src/containers/AboutPage/AboutPage.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import config from '../../config';
+import { twitterPageURL } from '../../util/urlHelpers';
+import { StaticPage, TopbarContainer } from '../../containers';
+import {
+  LayoutSingleColumn,
+  LayoutWrapperTopbar,
+  LayoutWrapperMain,
+  LayoutWrapperFooter,
+  Footer,
+  ExternalLink,
+} from '../../components';
+
+import css from './AboutPage.css';
+import image from './about-us-1056.jpg';
+
+const AboutPage = () => {
+  const { siteTwitterHandle, siteFacebookPage } = config;
+  const siteTwitterPage = twitterPageURL(siteTwitterHandle);
+
+  // prettier-ignore
+  return (
+    <StaticPage
+      title="About Us"
+      schema={{
+        '@context': 'http://schema.org',
+        '@type': 'AboutPage',
+        description: 'About Saunatime',
+        name: 'About page',
+      }}
+    >
+      <LayoutSingleColumn>
+        <LayoutWrapperTopbar>
+          <TopbarContainer />
+        </LayoutWrapperTopbar>
+
+        <LayoutWrapperMain className={css.staticPageWrapper}>
+          <h1 className={css.pageTitle}>Team building done right.</h1>
+          <img className={css.coverImage} src={image} alt="My first ice cream." />
+
+          <div className={css.contentWrapper}>
+            <div className={css.contentSide}>
+              <p></p>
+            </div>
+
+            <div className={css.contentMain}>
+              <h2>
+              Discover nearby team building activities and experiences, we have the right activity for every budget.
+              </h2>
+
+              <p>
+              Have questions about the activity, group size, or anything else? Use our messaging tool to communicate directly with providers. Our seamless booking experience allows you to submit your request, payment, and receive confirmation in just a few clicks. You can search for activities, compare prices, request to book, and easily pay through our platform. Our providers are passionate about strengthening your team and providing the best experience possible.
+              </p>
+
+              <h3 className={css.subtitle}>Are you a team building activity provider?</h3>
+
+              <p>
+              We would love to help you generate more bookings and increase your revenue. Get in touch with us <a href="mailto:hello@teamful.co">here</a>
+
+              </p>
+              <p>
+                You can also checkout our{' '}
+                <ExternalLink href={siteFacebookPage}>Facebook</ExternalLink> and{' '}
+                <ExternalLink href={siteTwitterPage}>Twitter</ExternalLink>.
+              </p>
+            </div>
+          </div>
+        </LayoutWrapperMain>
+
+        <LayoutWrapperFooter>
+          <Footer />
+        </LayoutWrapperFooter>
+      </LayoutSingleColumn>
+    </StaticPage>
+  );
+};
+
+export default AboutPage;
